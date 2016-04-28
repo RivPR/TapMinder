4c762ee48f411ad683f40aa63808b4b95da27377
Alex Peterson <alexander.peterson89@gmail.com>
2016-04-28 11:43:38 -0600

    working on entities

---
+++ JPATapMinder/src/entities/User.java
@@ -0,0 +1,89 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+
+	@Id
+	private int id;
+	@Column(name="firstname")
+	private String firstname;
+	@Column(name="lastname")
+	private String lastname;
+	@Column(name="email")
+	private String email;
+	@Column(name="password")
+	private String password;
+	
+
+	private UserType usertype;
+	
+	public User(){
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getUser_type_id() {
+		return user_type_id;
+	}
+
+	public void setUser_type_id(int user_type_id) {
+		this.user_type_id = user_type_id;
+	}
+
+	public UserType getUsertype() {
+		return usertype;
+	}
+
+	public void setUsertype(UserType usertype) {
+		this.usertype = usertype;
+	}
+	
+	
+	
+	
+}
---
+++ JPATapMinder/src/entities/UserType.java
@@ -0,0 +1,13 @@
+package entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_type")
+public class UserType {
+	
+	
+	
+
+}
