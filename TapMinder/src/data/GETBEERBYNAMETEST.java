274dbc4bd1d8e92dc4ed4f5e51f20d19d57491dc
Alex Peterson <alexander.peterson89@gmail.com>
2016-04-29 14:21:54 -0600

    trying to run a main program on a jpa dao thing is not a good idea :(

--- TapMinder/src/data/GETBEERBYNAMETEST.java
+++ TapMinder/src/data/GETBEERBYNAMETEST.java
@@ -2,12 +2,18 @@ package data;
 
 import java.util.List;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import entities.Beer;
 
 public class GETBEERBYNAMETEST {
-	static TapMinderDBDAO dao = new TapMinderDBDAO();
+	static TapMinderDBDAO dao;
 	
 	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = 
+	             new ClassPathXmlApplicationContext("tapminder-servlet.xml");
+		dao = context.getBean(TapMinderDBDAO.class,"dao");
+		
 		BeerParameters bp = new BeerParameters();
 		bp.setName("Vanilla porter");
 		List<Beer> beerList = dao.getBeers(bp);
