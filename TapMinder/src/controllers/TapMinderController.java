33b27e9e06be9396bb2d3c7ee188026af3873cb7
Angel M. Rivera Moreira <rivera.moreira@gmail.com>
2016-04-29 10:56:59 -0600

    Added the src/controllers directories and the controller class

---
+++ TapMinder/src/controllers/TapMinderController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Controller
+public class TapMinderController {
+	@Autowired
+	private TapMinderDAO tmDAO;
+
+}
