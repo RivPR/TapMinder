f1247d11113a2d43d307fc57d7665e0ae573339b
Alex Peterson <alexander.peterson89@gmail.com>
2016-05-01 14:30:07 -0600

    added java servlet and jstl dependencies to pom.xml

--- TapMinder/src/controllers/AlexTestController.java
+++ TapMinder/src/controllers/AlexTestController.java
@@ -1,7 +1,23 @@
 package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import data.TapMinderDBDAO;
+
 /*
  * I just made this to test out the stuff we learned in class
  */
+@Controller
 public class AlexTestController {
+	
+	@Autowired
+	TapMinderDBDAO dao;
+	
+	
 
+	
+	
+	
+	
 }
