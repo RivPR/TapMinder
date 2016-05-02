4201085f9e8b0e661450c69f6dfd54bd98782d13
Alex Peterson <alexander.peterson89@gmail.com>
2016-05-02 09:51:29 -0600

    added search beers page - not functional

--- TapMinder/WebContent/searchBeer.jsp
+++ TapMinder/WebContent/searchBeer.jsp
@@ -1,5 +1,10 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -7,21 +12,46 @@
 <title>Insert title here</title>
 </head>
 <body>
+		<c:if test="${searchSetting == ''}">
+			<form name="searchBy" action="findBeersPage.do" method="GET">
+				<select name="findBy">
+					<option value="">SELECT ONE</option>
+					<option value="name">Name</option>
+					<option value="abv">ABV</option>
+					<option value="hop">Hop Count</option>
+					<option value="style">Style</option>
+					<option value="rating">Rating</option>
+				</select>
 
-<form name="searchBy" action="findBeersPage.do" method="GET">
-	<select name="findBy">
-		<option value="">SELECT ONE</option>
-		<option value="name">Name</option>
-		<option value="abv">ABV</option>
-		<option value="hop">Hop Count</option>
-		<option value="style">Style</option>
-		<option value="rating">Rating</option>
-	</select>
-	
-	<button type="submit">GO SEARCH</button>
+				<button type="submit">GO SEARCH</button>
 
-</form>
+			</form>
+		</c:if>
+
+		<form:form action="searchBeers.do" modelAttribute="BeerParameters">
+	<c:choose>
+		
+		<c:when test="${searchSetting == 'name' }">
+			BEER NAME <form:input type="text" path="name" placeholder="Beer name"/>
+		</c:when>
+		<c:when test="${searchSetting == 'abv' }">
+ABV from <form:input type="text" path="abvLow" /> to <form:input type="text" path="abvHigh" />
+		</c:when>
+		<c:when test="${searchSetting == 'hop' }">
+Hop count from <form:input type="text" path="hopCountLow" /> to <form:input type="text" path="hopCountHigh" />
+		</c:when>
+		<c:when test="${searchSetting == 'style' }">
+Style <form:input type="text" path="style" placeholder="style"/>
+
+		</c:when>
+		<c:when test="${searchSetting ==  'rating'}">
+Rating <form:input type="text" path="rating" placeholder="Number 1-5" />
+		</c:when>
 
 
+	</c:choose>
+		
+		</form:form>
+		
 </body>
 </html>
--- TapMinder/src/controllers/TapMinderController.java
+++ TapMinder/src/controllers/TapMinderController.java
@@ -17,54 +17,56 @@ import entities.User;
  * I just made this to test out the stuff we learned in class
  */
 @Controller
-@SessionAttributes({"currentUser"})
+@SessionAttributes({ "currentUser" })
 public class TapMinderController {
 
 	@ModelAttribute("currentUser")
-	private User setBlankCurrentUser(){
+	private User setBlankCurrentUser() {
 		User user = new User();
 		return user;
 	}
-	
+
 	@Autowired
 	private TapMinderDAO dao;
 
 	@RequestMapping("menu.do")
-	private ModelAndView menu(@RequestParam("menuChoice") String menuChoice, @ModelAttribute("currentUser") User currentUser) {
+	private ModelAndView menu(@RequestParam("menuChoice") String menuChoice,
+			@ModelAttribute("currentUser") User currentUser) {
 		ModelAndView mv = new ModelAndView();
 
 		// menu actions mapped below
 		switch (menuChoice) {
 		case "login":
-			mv.addObject("user",new User());
+			mv.addObject("user", new User());
 			mv.setViewName("AlexTestJSPStuff/login.jsp");
 			break;
-			
+
 		case "findBeers":
+			mv.addObject("searchSetting", "");
 			mv.setViewName("searchBeer.jsp");
 			break;
 		case "findBreweries":
-			//TODO: add real stuff
+			// TODO: add real stuff
 			mv.setViewName("index.jsp");
 			break;
 		case "viewUserBeers":
-			//TODO: add real stuff
+			// TODO: add real stuff
 			mv.setViewName("index.jsp");
 			break;
 		case "myAccount":
-			//TODO: add real stuff
+			// TODO: add real stuff
 			mv.setViewName("index.jsp");
 			break;
 		case "modifyBreweries":
-			//TODO: add real stuff
+			// TODO: add real stuff
 			mv.setViewName("index.jsp");
 			break;
 		case "modifyModerators":
-			//TODO: add real stuff
+			// TODO: add real stuff
 			mv.setViewName("index.jsp");
 			break;
 		case "modifyUsers":
-			//TODO: add real stuff
+			// TODO: add real stuff
 			mv.setViewName("index.jsp");
 			break;
 		case "logout":
@@ -72,7 +74,7 @@ public class TapMinderController {
 			System.out.println(currentUser);
 			currentUser = new User();
 			System.out.println(currentUser);
-			mv.addObject("currentUser",currentUser);
+			mv.addObject("currentUser", currentUser);
 			mv.setViewName("indexAlexTest.jsp");
 			break;
 		default:
@@ -83,38 +85,48 @@ public class TapMinderController {
 
 		return mv;
 
-	}//menu.do
-	
+	}// menu.do
+
 	@RequestMapping("findBeersPage.do")
-	private ModelAndView findBeers(@RequestParam("findBy") String choice){
-		
+	private ModelAndView findBeers(@RequestParam("findBy") String choice) {
+
 		ModelAndView mv = new ModelAndView();
 		String searchSetting = choice;
-		mv.addObject("searchSetting",choice);
+		mv.addObject("searchSetting", choice);
+		mv.addObject("BeerParameters",new BeerParameters());
 		mv.setViewName("searchBeer.jsp");
-	
+
 		return mv;
 	}
-	
+
+	@RequestMapping("searchBeers.do")
+	private ModelAndView searchBeers(BeerParameters beerParameters) {
+		ModelAndView mv = new ModelAndView();
+		System.out.println("lsdkjfa;slkdsa;dlka;sodjfsal;kdfyayayyayayayayayay");
+		
+		mv.setViewName("indexAlexTest.jsp");
+		
+		return mv;
+	}
+
 	@RequestMapping("login.do")
-	private ModelAndView login(User user, @ModelAttribute("currentUser") User currentUser){
+	private ModelAndView login(User user, @ModelAttribute("currentUser") User currentUser) {
 		ModelAndView mv = new ModelAndView();
 		System.out.println(user.getEmail());
 		System.out.println(user.getPassword());
 		LoginResult result = dao.getUserByLoginCredentials(user);
-		if(result.getUser() != null){
-		System.out.println("new current user: " + currentUser);
-		//TODO: better way to set session attributes?
-		mv.addObject("currentUser",result.getUser());
-		mv.setViewName("indexAlexTest.jsp");
-		}
-		else{
-			mv.addObject("LoginError",result.getMessage());
-			mv.addObject("user",new User());
+		if (result.getUser() != null) {
+			System.out.println("new current user: " + currentUser);
+			// TODO: better way to set session attributes?
+			mv.addObject("currentUser", result.getUser());
+			mv.setViewName("indexAlexTest.jsp");
+		} else {
+			mv.addObject("LoginError", result.getMessage());
+			mv.addObject("user", new User());
 			mv.setViewName("AlexTestJSPStuff/login.jsp");
 		}
 		return mv;
-		
+
 	}
 
 }
